8b2e19d67550bf3a95e5d82110921a5a2667e4e0
nickyyyG <ng477618@ohio.edu>
2022-09-12 19:11:24 -0400

    Adding program file

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/ git-lab-2
+
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nicole 
+ *        Date: 9/12/22
+ * Description: "Hello Git!" upload to online repo
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
